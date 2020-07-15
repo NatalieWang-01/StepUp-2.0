7772331f60c77d5954bf4b0d68381ad3ee0dbadf
NatalieWang-01 <wangnatalie101@gmail.com>
2020-07-15 14:12:28 -0500

    New project

---
+++ StepUp 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StepUp 2.0
+//
+//  Created by Natalie Wang on 7/15/20.
+//  Copyright © 2020 Natalie Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
